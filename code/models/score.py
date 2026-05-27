735117a9f3066b49b0cb54e4b05cc3eb2cd3f046
Omar Loya <l24410173@itparral.edu.mx>
2026-05-26 18:13:00 -0600

    Prueba score

--- code/models/score.py
+++ code/models/score.py
@@ -1,19 +1,19 @@
-"""Score data model."""
-
 from datetime import datetime
 
 
 class Score:
-    def __init__(self, players: str, score: int, duration: int, date: str):
-        self.players = players
+    def __init__(self, player_name: str, character: str, score: int, duration: int, date: str):
+        self.player_name = player_name
+        self.character = character
         self.score = int(score)
         self.duration = int(duration)
         self.date = date
 
     @classmethod
-    def create(cls, players: str, score: int, duration: int):
+    def create(cls, player_name: str, character: str, score: int, duration: int):
         return cls(
-            players=players,
+            player_name=player_name,
+            character=character,
             score=int(score),
             duration=int(duration),
             date=datetime.now().strftime("%Y-%m-%d %H:%M"),
@@ -21,7 +21,8 @@ class Score:
 
     def to_dict(self) -> dict:
         return {
-            "players": self.players,
+            "player_name": self.player_name,
+            "character": self.character,
             "score": self.score,
             "duration": self.duration,
             "date": self.date,
