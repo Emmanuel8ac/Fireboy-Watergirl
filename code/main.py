e29461f125edfa672137b58da0c46f5424dfc77d
Omar Loya <l24410173@itparral.edu.mx>
2026-05-28 16:56:54 -0600

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,12 +1,14 @@
+# Importa y organiza las herramientas necesarias
 import sys
+
 from PySide6.QtWidgets import QApplication
-from main_window import MainWindow
+
+from ui.main_window import MainWindow
 
 
+# Inicia la aplicación
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = MainWindow()
     window.show()
-
     sys.exit(app.exec())
